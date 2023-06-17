e2c1a0d30914b04b132311c70632a59f4ed189f7
Filipe Bacof <filipebacof@gmail.com>
2023-06-17 16:00:49 -0300

    :sparkles: FEAT questions + methods for answers

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,14 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
+import questions from '../QuestionsDB'
 
 export const GET = (req: NextRequest, res: NextResponse) => {
-  const { searchParams } = new URL(req.url)
+  // const { searchParams } = new URL(req.url)
   // http://localhost:3000/api/questions?id=33
-  const id = searchParams.get('id')
+  // const id = searchParams.get('id')
   return NextResponse.json({
     status: 200,
-    body: {
-      name: 'John Doe',
-      id,
-    },
+    body: questions[0],
   })
 }
--- src/model/answer.ts
+++ src/model/answer.ts
@@ -3,10 +3,18 @@ export default class AnswerModel {
   #correct: boolean
   #revealed: boolean
 
-  constructor(value: string, correct: boolean, revealed: false) {
+  constructor(value: string, correct: boolean, revealed = false) {
     this.#value = value
     this.#correct = correct
     this.#revealed = revealed
+  }
+
+  static right(value: string) {
+    return new AnswerModel(value, true)
+  }
+
+  static wrong(value: string) {
+    return new AnswerModel(value, false)
   }
 
   get value() {
