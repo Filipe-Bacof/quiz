e0623bfba3814cc60c5ce481fdcb1a993a826f53
Filipe Bacof <filipebacof@gmail.com>
2023-06-17 16:19:49 -0300

    :sparkles: FEAT questions returned on API

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -2,11 +2,30 @@ import { NextRequest, NextResponse } from 'next/server'
 import questions from '../QuestionsDB'
 
 export const GET = (req: NextRequest, res: NextResponse) => {
-  // const { searchParams } = new URL(req.url)
+  const { searchParams } = new URL(req.url)
   // http://localhost:3000/api/questions?id=33
-  // const id = searchParams.get('id')
-  return NextResponse.json({
-    status: 200,
-    body: questions[0].convertToObject(),
-  })
+  const id = Number(searchParams.get('id'))
+
+  if (id) {
+    const questionByIdOrNothing = questions.filter(
+      (question) => question.id === id,
+    )
+
+    if (questionByIdOrNothing.length === 1) {
+      const selectedQuestion = questionByIdOrNothing[0]
+      return NextResponse.json({
+        status: 200,
+        body: selectedQuestion.convertToObject(),
+      })
+    } else {
+      return NextResponse.json({
+        status: 204,
+      })
+    }
+  } else {
+    return NextResponse.json({
+      status: 200,
+      body: questions.map((question) => question.convertToObject()),
+    })
+  }
 }
