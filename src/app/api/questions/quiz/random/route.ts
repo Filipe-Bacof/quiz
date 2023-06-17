bdd30c79fc3b113bc6fa55ff2b74cf8cc175cb1e
Filipe Bacof <filipebacof@gmail.com>
2023-06-17 18:01:42 -0300

    :recycle: REFACTOR: shuffle function used on API

--- src/app/api/questions/quiz/random/route.ts
+++ src/app/api/questions/quiz/random/route.ts
@@ -1,17 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 import questions from '../../../QuestionsDB'
+import { shuffle } from '@/functions/shuffle'
 
 export const GET = (req: NextRequest, res: NextResponse) => {
   // http://localhost:3000/api/questions/quiz/random
 
-  const numbers = questions.map((question) => question.id)
-
-  const randomNumbers = numbers
-    .map((num) => ({ value: num, random: Math.random() }))
-    .sort((obj1, obj2) => obj1.random - obj2.random)
+  const ids = questions.map((question) => question.id)
 
   return NextResponse.json({
     status: 200,
-    body: randomNumbers.map((num) => num.value),
+    body: shuffle(ids),
   })
 }
